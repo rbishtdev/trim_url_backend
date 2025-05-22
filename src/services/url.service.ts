5cf4c2465f74e070993ba3b43eaf9ac6eb210ebf
Rajendra Singh Bisht <rajanvisa2001@gmail.com>
2025-05-22 08:50:45 +0530

    chore: use the same url shortCode if its available in db

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -2,13 +2,45 @@ import prisma from '../db/db';
 import {generateShortCode} from "../helpers/shortCodeHelper";
 
 export const createShortUrlService = async (targetUrl: string, userId: string | null) => {
+    let existingUrl;
+    console.log(userId);
+    if (userId) {
+        existingUrl = await prisma.url.findFirst({
+            where: {
+                targetUrl,
+                userId,
+                deleted: false,
+            },
+        });
+        console.log(existingUrl);
+    } else {
+        existingUrl = await prisma.url.findFirst({
+            where: {
+                targetUrl,
+                userId: {
+                    equals: null,
+                },
+                deleted: false,
+            },
+        });
+        console.log(existingUrl);
+
+    }
+
+    if (existingUrl) {
+        return {
+            shortCode: existingUrl.shortCode,
+            targetUrl: existingUrl.targetUrl,
+        };
+    }
+
     const shortCode = await generateUniqueShortCode();
 
     const newUrl = await prisma.url.create({
         data: {
             shortCode,
             targetUrl,
-            userId: userId || undefined,
+            userId: userId ?? null,
         },
     });
 
